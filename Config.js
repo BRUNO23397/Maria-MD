fff1ba72e7117156d48411b1e757a774c8ebe99d
BRUNO23397 <165482237+BRUNO23397@users.noreply.github.com>
2024-03-30 14:14:32 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['07036169234']
+global.ownername = "JØ~ÈŁ♧"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Ngerian"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'ZEALOT-EZ' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = 'XxJOELxX'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'ZEALOT-EZ'
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
@@ -22,7 +22,7 @@ global.wm = "Ayush botz inc."
 global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
 
 //custom prefix
-global.prefa = ['.']
+global.prefa = ['*']
 
 //false=disable and true=enable
 global.welcome = false //auto welcome
